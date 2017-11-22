f3777b31d2e722db215fe5b241b59a50e9e608d9
Oleksandr Kirichenko <numen31337@gmail.com>
2017-11-22 14:11:51 +0100

    Initial commit

---
+++ Day/Day.h
@@ -0,0 +1,19 @@
+//
+//  Day.h
+//  Day
+//
+//  Created by Oleksandr Kirichenko on 11/22/17.
+//  Copyright © 2017 Oleksandr Kirichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Day.
+FOUNDATION_EXPORT double DayVersionNumber;
+
+//! Project version string for Day.
+FOUNDATION_EXPORT const unsigned char DayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Day/PublicHeader.h>
+
+
